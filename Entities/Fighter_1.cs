0c4a3ccdad6bc0999753c7653e0aa898621ded08
jonathandilsook <30672188+jonathandilsook@users.noreply.github.com>
2018-07-03 18:24:55 +1200

    Add files via upload
     Street fighter game project cs files.

---
+++ Entities/Fighter_1.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fighter : MonoBehaviour {
+	public enum PlayerType
+	{
+		HUMAN, AI	
+	};
+
+	public static float MAX_HEALTH = 100f;
+
+	public float healt = MAX_HEALTH;
+	public string fighterName;
+	public Fighter oponent;
+
+	public PlayerType player;
+	public FighterStates currentState = FighterStates.IDLE;
+
+	protected Animator animator;
+	private Rigidbody myBody;
+	private AudioSource audioPlayer;
+	
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody> ();
+		animator = GetComponent<Animator> ();
+		audioPlayer = GetComponent<AudioSource> ();
+	}
+
+	public void UpdateHumanInput (){
+		if (Input.GetAxis ("Horizontal") > 0.1) {
+			animator.SetBool ("WALK", true);
+		} else {
+			animator.SetBool ("WALK", false);
+		}
+
+		if (Input.GetAxis ("Horizontal") < -0.1) {
+			animator.SetBool ("WALK_BACK", true);
+		} else {
+			animator.SetBool ("WALK_BACK", false);
+		}
+
+		if (Input.GetAxis ("Vertical") < -0.1) {
+			animator.SetBool ("DUCK", true);
+		} else {
+			animator.SetBool ("DUCK", false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			animator.SetTrigger("JUMP");
+		} 
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			animator.SetTrigger("PUNCH");
+		}
+
+		if (Input.GetKeyDown (KeyCode.K)) {
+			animator.SetTrigger("KICK");
+		}
+
+		if (Input.GetKeyDown (KeyCode.H)) {
+			animator.SetTrigger("HADOKEN");
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat ("health", healtPercent);
+
+		if (oponent != null) {
+			animator.SetFloat ("oponent_health", oponent.healtPercent);
+		} else {
+			animator.SetFloat ("oponent_health", 1);
+		}
+
+		if (player == PlayerType.HUMAN){
+			UpdateHumanInput();
+		}
+	}
+	public void playSound(AudioClip sound){
+		GameUtils.playSound (sound, audioPlayer);
+	}
+
+
+	public float healtPercent {
+		get {
+			return healt / MAX_HEALTH;
+		}	
+	}
+
+	public Rigidbody body {
+		get {
+			return this.myBody;
+		}
+	}
+}
---
+++ Entities/Hadoken_1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hadoken : MonoBehaviour {
+	public Fighter caster;
+
+	private Rigidbody body;
+	private float creationTime;
+
+	// Use this for initialization
+	void Start () {
+		creationTime = Time.time;
+		body = GetComponent<Rigidbody> ();
+		body.AddRelativeForce (new Vector3 (200, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - creationTime > 3) {
+			Destroy(gameObject);
+		}
+	}
+}
